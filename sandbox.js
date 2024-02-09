95693407029fa7badfa831783f8dbce9c71ca762
Christopher Abate <contact@christopherabate.com>
2024-02-09 09:57:46 +0100

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,85 @@
+class Sandbox {
+  
+  constructor(options = {}) {
+    
+    this.selector = options.selector || ".sandbox";
+    this.editable = options.editable || false;
+    this.callback = options.callback || false;
+    
+    this.sandbox = document.querySelector(this.selector);
+    this.output = this.sandbox.querySelector("iframe");
+    this.template = this.output.textContent;
+    
+    this.updateOutput();
+    
+    if (this.editable === true) {
+      
+      let codeboxes = this.sandbox.querySelectorAll("pre code");
+    
+      codeboxes.forEach((codebox) => {
+      
+        let editor = document.createElement("div");
+        editor.classList.add("sandbox-editor");
+        
+        let textarea = document.createElement("textarea");
+        textarea.setAttribute("rows", "1");
+        textarea.setAttribute("spellcheck", "false");
+        textarea.setAttribute("autocorrect", "off");
+        textarea.setAttribute("autocomplete", "off");
+        textarea.setAttribute("autocapitalize", "off");
+        textarea.setAttribute("translate", "no");
+        textarea.setAttribute("title", codebox.getAttribute("title"));
+        textarea.setAttribute("aria-label", codebox.getAttribute("title"));
+        textarea.innerHTML = codebox.textContent;
+        
+        codebox.parentElement.insertAdjacentElement("beforebegin", editor);
+        editor.appendChild(codebox.parentElement);
+        editor.appendChild(textarea);
+        
+        let timeoutID;
+        
+        textarea.addEventListener("input", (e) => {
+          
+          codebox.textContent = e.target.value;
+          
+          if (this.callback !== false) {this.tryCallback(this.callback)}
+          
+          clearTimeout(timeoutID);
+          timeoutID = setTimeout(() => {
+            this.updateOutput()
+          }, 400);
+        })
+      });
+    }
+  }
+  
+  updateOutput() {
+    
+    let input = {
+      html: this.sandbox.querySelector("code[class*='html']").textContent,
+      css: this.sandbox.querySelector("code[class*='css']").textContent,
+      js: this.sandbox.querySelector("code[class*='js']").textContent,
+    };
+    
+    let languages = Object.keys(input);
+    let code = Object.values(input);
+    
+    let render = new Function(...languages, `return \`${this.template}\`;`)(...code);
+		let clone = this.output.cloneNode();
+		this.output.replaceWith(clone);
+		this.output = clone;
+    
+    this.output.contentWindow.document.open();
+    this.output.contentWindow.document.writeln(render);
+    this.output.contentWindow.document.close();
+  }
+  
+  tryCallback(callback) {
+    
+    try {
+      callback();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
